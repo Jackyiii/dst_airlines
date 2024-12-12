869dd2ea54fdcfb3b289e9437b2de570d7fcd5b5
ASSOUMANE-Tahirou <t.assoumane07@gmail.com>
2024-12-12 14:58:17 +0100

    database created excep for aircraft

--- project/start.sh
+++ project/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Starting main.py" > /log/output.log
 python main.py >> /log/output.log 2>&1
+#####test
+export POSTGRES_USER=myuser
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_DB=mydatabase
+export POSTGRES_PASSWORD=mypassword
+
+# Démarrer PostgreSQL avec Docker (par exemple)
+docker run --name postgres-container -e POSTGRES_USER -e POSTGRES_PASSWORD -e POSTGRES_DB -d postgres
