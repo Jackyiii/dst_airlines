6057a9417428e406111217974862c1b072f440ed
Herman <nguimdjioherman@gmail.com>
2025-04-07 14:52:03 +0200

    maj_dockercompose+requirement
    Creation de API avec le script api_ml.py , creation du dossier models , maj du requirement.txt pour resoudre les problèmes de conflits entre le numpy qui a servi pour modèle random_forest et celui de docker , maj du docker compose

--- project/api_ml.py
+++ project/api_ml.py
@@ -4,39 +4,37 @@ import pandas as pd
 import os
 from pydantic import BaseModel
 
-# Charger le modèle depuis le fichier sauvegardé
-MODEL_PATH = os.path.join(os.path.expanduser("~"), "Documents", "random_forest.pkl")  
+# Chemin du modèle dans le container Docker
+MODEL_PATH = os.path.join("models", "random_forest.pkl")
+
+# Vérifier que le modèle existe
+if not os.path.exists(MODEL_PATH):
+    raise FileNotFoundError(f"Le fichier du modèle est introuvable à l'emplacement : {MODEL_PATH}")
+
+# Charger le modèle
 model = joblib.load(MODEL_PATH)
 
-# Initialisation de l'application FastAPI
+# FastAPI init
 app = FastAPI(title="API de Prédiction de Retard de Vols")
 
-# Définition du format des données d'entrée à l'API
 class FlightData(BaseModel):
-    DepartureDayOfWeekNumber: str  # Jour de départ en semaine (ex: "1" pour lundi)
-    DepartureAirportCode: str  # Code de l'aéroport de départ (ex: "JFK")
-    ArrivalAirportCode: str  # Code de l'aéroport d'arrivée (ex: "LAX")
-    AircraftCode: str  # Code de l'avion utilisé (ex: "A320")
-    FlightStatusCode: str  # Code de statut du vol (ex: "LD")
-    DaysOfOperation: str  # Jours d'opération du vol
-    ActualDepartureDayOfWeekNumber: str  # Jour réel du départ en semaine
-    ActualArrivalDayOfWeekNumber: str  # Jour réel de l'arrivée en semaine
-    ScheduledDepartureDayOfWeekNumber: str  # Jour programmé du départ
-    ScheduledArrivalDayOfWeekNumber: str  # Jour programmé de l'arrivée
+    DepartureDayOfWeekNumber: str
+    DepartureAirportCode: str
+    ArrivalAirportCode: str
+    AircraftCode: str
+    FlightStatusCode: str
+    DaysOfOperation: str
+    ActualDepartureDayOfWeekNumber: str
+    ActualArrivalDayOfWeekNumber: str
+    ScheduledDepartureDayOfWeekNumber: str
+    ScheduledArrivalDayOfWeekNumber: str
     DurationMinutes: float  
-    
-# Définition de la route API pour la prédiction du retard des vols
+
 @app.post("/predict")
 def predict_delay(data: FlightData):
     try:
-        # Conversion des données d'entrée en DataFrame Pandas
         df_input = pd.DataFrame([data.dict()])
-        
-        # Effectuer la prédiction avec le modèle chargé
         prediction = model.predict(df_input)
-        
-        # Retourner la prédiction sous forme de JSON
         return {"prediction": int(prediction[0])}
     except Exception as e:
-        # Gestion des erreurs et retour d'une réponse HTTP 500 en cas de problème
         raise HTTPException(status_code=500, detail=f"Erreur lors de la prédiction: {str(e)}")
--- project/database/aircraftdata.sql
+++ project/database/aircraftdata.sql
@@ -4,4 +4,4 @@ CREATE TABLE aircraft (
     LanguageCode VARCHAR(10),
     AircraftName VARCHAR(255),
     AirlineEquipCode VARCHAR(50)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
